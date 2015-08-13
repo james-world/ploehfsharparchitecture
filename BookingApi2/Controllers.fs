4396139c7c020dd9d8955e07ddebd05695a56903
James World <james-world@users.noreply.github.com>
2015-08-14 00:47:45 +0100

    Basic Setup

---
+++ BookingApi2/Controllers.fs
@@ -0,0 +1,11 @@
+﻿namespace BookingApi2
+
+open System.Web.Http
+open System.Net
+
+type HomeController() =
+    inherit ApiController()
+
+    [<Route("home")>]
+    member this.Get() : IHttpActionResult =
+        this.Ok() :> _
